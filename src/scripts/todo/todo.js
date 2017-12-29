0673f726e3d88cf52c998b5fc6e8ab0555804199
Yurii Levantovych <domovoj1@gmail.com>
2017-12-29 12:40:42 +0200

    Remove counters, changed removing todo functionality

--- src/scripts/todo/todo.js
+++ src/scripts/todo/todo.js
@@ -1,83 +1,98 @@
 // 1. Implement deleting
 // 2. Display counts
 const todos = [
-  {
-    id: 1,
-    value: "Купити квитки",
-    isChecked: false
-  },
-  {
-    id: 2,
-    value: "Поїхати додому",
-    isChecked: true
-  },
-  {
-    id: 3,
-    value: "Замовити таксі",
-    isChecked: false
-  }
-];
+    {
+        id:        1,
+        value:     "Купити квитки",
+        isChecked: false
+    },
+    {
+        id:        2,
+        value:     "Поїхати додому",
+        isChecked: true
+    },
+    {
+        id:        3,
+        value:     "Замовити таксі",
+        isChecked: false
+    }
+]
 
 
-const todoAppView = document.getElementById("todo-list");
+const todoAppView = document.getElementById("todo-list")
 
 const changeStatusById = todoId => {
-  const todo = todos.find(todo => todo.id === todoId);  // not anderstood
-  todo.isChecked = !todo.isChecked;
-};
+    const todo = todos.find(todo => todo.id === todoId);
+    todo.isChecked = !todo.isChecked;
+}
 
- //not anderstood
 const getTodoHTML = (item, index) => `<li>
     <input type="checkbox" id="todo${index}" ${item.isChecked
-  ? "checked"
-  : ""} onChange="changeStatusById(${item.id})"/>             
+    ? "checked"
+    : ""} onChange="changeStatusById(${item.id})"/>             
     <label for="todo${index}">${item.value}</label>
-    <button onclick="removeElement()">X</button>
-  </li>`;
+    <button onclick="removeTodoById(${item.id})">X</button>
+  </li>`
 
 const renderTodoList = () => {
-  todoAppView.innerHTML = todos.map(getTodoHTML).join("");
-};
+    todoAppView.innerHTML = todos.map(getTodoHTML).join("")
+    // todoAppView.innerHTML = (function () {
+    //     let todosHtml = ''
+    //     todos.forEach((item, index) => {
+    //         todosHtml += getTodoHTML(item, index)
+    //     })
+    //     return todosHtml
+    // })();
+}
 
-let counter =0;
+// Why do you need forEach
+// todos.forEach((item) => {
+//     item.isChecked = true;
+// })
+
+// let counter = 0
 const addTodo = () => {
-  const input = document.getElementById("input").value;
-  const doneCount = document.getElementById('doneCount');
-  counter += 1
-  doneCount.innerText = counter;
- 
- 
-  todos.push({
-    id: todos.length + 1,
-    value: input,
-    isChecked: false
-  });
-  todoAppView.innerHTML = '';
-
-  renderTodoList();
-  return false;
-};
-
-renderTodoList();
+    const input = document.getElementById("input").value
+    // const doneCount = document.getElementById('doneCount')
+    // counter += 1
+    // doneCount.innerText = counter
 
 
+    todos.push({
+        id:        todos.length + 1,
+        value:     input,
+        isChecked: false
+    })
+    // @todo remove it
+    // todoAppView.innerHTML = ''
 
-    //  removeElement
-    let reverscounter = counter;  // counter
-    const removeElement = () => {
-      reverscounter -=1;
-        const input = document.getElementById("input").value;
-        const undoneCount = document.getElementById('undoneCount');
-        undoneCount.innerText = reverscounter;
-        
-        todos.pop({
-          id: todos.length + 1,
-          value: input,
-          isChecked: false
-        });
-        renderTodoList();
-        return false;
-      };
-      
-     window.addTodo = addTodo;
-     window.removeElement = removeElement;
+    renderTodoList()
+    return false
+}
+
+renderTodoList()
+
+
+//  removeElement
+// @todo remove it
+// let reverscounter = counter  // counter
+const removeTodoById = todoId => {
+    // @tod remove it
+    // reverscounter -= 1
+    // const undoneCount = document.getElementById('undoneCount')
+    // undoneCount.innerText = reverscounter
+    const index = todos.findIndex(todo => todo.id === todoId);
+
+    todos.splice(index, 1)
+    renderTodoList()
+    return false
+}
+
+// const general = document.getElementById('generalCount');
+// let count = todos.length;
+// general.innerText = count;
+// console.log(general);
+
+window.addTodo = addTodo
+window.changeStatusById = changeStatusById
+window.removeTodoById = removeTodoById
